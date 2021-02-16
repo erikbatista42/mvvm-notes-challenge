81b64e1f16e8055fb405ea1ef3e42cd61773de66
erik <eirkbatista42@gmail.com>
2021-02-16 08:58:28 -0700

    added project

---
+++ CollectionViewMVVM/ViewModel/DessertCellViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ItemCellModel.swift
+//  CollectionViewMVVM
+//
+//  Created by Adriana González Martínez on 5/5/20.
+//  Copyright © 2020 Adriana González Martínez. All rights reserved.
+//
+
+import Foundation
+
+struct DessertCellViewModel {
+    let descriptionText: String
+    var displayPrice: String?
+    let price: Double
+    
+    mutating func getDisplayText() -> String?{
+        //TODO: format the text to return the image and price with a $ sign and two decimal numbers
+        // Note to update value of display price
+        return "\(descriptionText) $" + String(format: "%.2f", price)
+    }
+}
